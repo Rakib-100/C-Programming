e462ada13aea2c0ff434c39b4911a8feaedaccd1
MD Rakib <rakibhowlader810@gmail.com>
2026-03-06 02:28:36 +0600

    project 1st push

---
+++ DIU Student Accommodation Management System(DIU-SAMS)/DIU-SAMS.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+typedef struct hostel
+{
+    char name[55];
+    int hid;
+    int mob;
+    int ts;
+    int fs;
+    int sf;
+    int dfd;
+    char gl[505];
+    char otinfo[1005];
+    struct hostel *next;
+
+
+
+}H;
+H *head=NULL;
+void addhostel(char namec[50],int hidc,int mobc,int tsc,)
+
+
+
+
