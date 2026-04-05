f38b590b3da7d05693a3459a96d2ef7f7a0f1514
Rakib-100 <rakibhowlader810@gmail.com>
2026-04-05 21:48:29 +0600

    file add

--- DIU Student Accommodation Management System(DIU-SAMS)/DIU-SAMS.c
+++ DIU Student Accommodation Management System(DIU-SAMS)/DIU-SAMS.c
@@ -1,3 +1,4 @@
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
@@ -18,8 +19,22 @@ typedef struct hostel
 
 
 }H;
+
+
+
+
+
+
 H *head=NULL;
 H *last=NULL;
+
+
+
+
+
+
+
+
 void addhostel()
 {
     H *hos=malloc(sizeof(H));
@@ -72,6 +87,12 @@ void addhostel()
 }
 
 
+
+
+
+
+
+
 void dishos()
 {
     H *temp=head;
@@ -99,6 +120,141 @@ void dishos()
     else{printf("\n**no record found**\n\n");}
 }
 
+
+
+
+
+
+
+void deleteHostel()
+{
+    int id,p=0;
+    printf("Enter Hostel ID to delete:\n");
+    scanf("%d", &id);
+
+    H *temp = head;
+    H *prev = NULL;
+
+    if (head==NULL)
+    {
+        printf("NO record found\n press enter to continue");
+        getchar();
+        return;
+    }
+
+    if(head->hid==id)
+    {
+      head=head->next;
+      free(temp);
+      printf("successfully deleated\n press enter to continue\n");
+      getchar();
+      getchar();
+      return;
+    }
+    while(temp!=NULL&&temp->hid!=id  )
+    {
+        prev=temp;
+        temp=temp->next;
+
+    }
+    if(temp==NULL)
+    {
+       printf("NO record found\n press enter to continue");
+        getchar();
+        return;
+    }
+
+        prev->next=temp->next;
+        if(prev->next==NULL)
+        {
+            last=prev;
+        }
+        free(temp);
+     printf("successfully deleated\n press enter to continue\n");
+      getchar();
+      getchar();
+      return;
+
+
+}
+
+
+
+
+
+
+
+void searchHostel()
+{
+    int id;
+    printf("Enter Hostel ID to search:\n");
+    scanf("%d", &id);
+
+    H *temp = head;
+    int found = 0;
+
+    while (temp != NULL)
+    {
+        if (temp->hid == id)
+        {
+            printf("\nHostel Found!\n");
+            printf("Hostel name:%s\n", temp->name);
+            printf("Hostel ID:%d\n", temp->hid);
+            printf("contact no:%d\n", temp->mob);
+            printf("Total seat:%d\n", temp->ts);
+            printf("free seat:%d\n", temp->fs);
+            printf("seat fee per month(TK): %d\n", temp->sf);
+            printf("distance from daffodil(KM): %d\n", temp->dfd);
+            printf("google map link:\n%s\n", temp->gl);
+            printf("other info:\n%s\n", temp->otinfo);
+
+            found = 1;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    if (found == 0)
+    {
+        printf("Hostel not found!\n");
+    }
+
+    printf("\nPress Enter to continue...");
+    getchar();
+    getchar();
+}
+
+
+
+
+void loadhostelinfo()
+{
+
+
+
+}
+
+
+
+
+
+void  savehostel()
+{
+   FILE *fp=fopen("allhos.txt","w");
+   H *temp=head;
+   while(temp!=NULL)
+   {
+       fprintf(fp, "%d %d %d %d %d %d\n",temp->hid, temp->mob, temp->ts,temp->fs, temp->sf, temp->dfd);
+       fprintf(fp, "%s\n%s\n%s\n", temp->name, temp->gl, temp->otinfo);
+       temp=temp->next;
+    }
+    fclose(fp);
+}
+
+
+
+
+
 int main()
 {
    int p;
@@ -114,6 +270,10 @@ int main()
     printf("ALL OPTION:\n\n");
     printf("1:Add your Hostel.\n");
     printf("2:display all Hostel info.\n");
+    printf("3:deleate a Hostel info.\n");
+    printf("4:search hostel by id.\n");
+    printf("8:Exit\n");
+
 
 
 
@@ -124,11 +284,11 @@ int main()
     {
         case 1:addhostel();break;
         case 2:dishos();break;
+        case 3:deleteHostel();break;
+        case 4:searchHostel();break;
 
     }
     }while(p!=8);
+    savehostel();
 
 }
-
-
-
